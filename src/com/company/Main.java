3bbdb1bc00adc9dbe71c9a9da37ac1b136e15f9e
marc572m <marc572m@edu.easj.dk>
2021-09-14 12:20:10 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+       /*
+
+Expression	Result
+yes == no || grade > amount	        True
+amount == 40.0 || 50.0              Error
+hiVal != loVal || loVal < 0         True
+True || hello.length() > 0          True
+hello.isEmpty() && yes              False
+grade <= 100 && !false              True
+!yes || no                          False
+grade > 75 > amount                 True
+amount <= hiVal && amount >= loVal  True
+no && !no || yes && !yes            False
+
+        */
+    }
+}
